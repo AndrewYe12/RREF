7f3e1d2a113e39aef8e620a774a98cfa0ae20d8e
Andrew Ye <andrewye@Andrews-MacBook-Pro-2.local>
2024-05-03 10:29:33 -0400

    tiny changes

--- execute.py
+++ execute.py
@@ -1,5 +1,7 @@
 from all_together import matrix
-import numpy as np 
+#import numpy as np 
 
-x = matrix([[-3,3,4,5], [2,2,3,3],[2,2,3,np.pi]])
-print(x.fraction().matrix) 
+x = matrix([[-3,3,4,5], [2,2,3,3],[2,2,3,22]])
+y = matrix([[-3,3,4,10,9], [2,22,3,3,8],[2,11,3,22,7],[4,2,1,34,5]])
+z = matrix([[-3,3,4], [2,2,3],[2,2,4]]) 
+print(z.det()) 
